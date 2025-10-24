60fcec258a6d4bbc481c8c83813dd33c90987968
Gom3rye <kimmyeongji3@gmail.com>
2025-10-24 10:27:34 +0900

    chore: edit main.tf

--- modules/eks-cluster/iam.tf
+++ modules/eks-cluster/iam.tf
@@ -1,5 +1,5 @@
 ######################################
-# EKS 클러스터 IAM 역할 생성
+# EKS Cluster IAM Role
 ######################################
 resource "aws_iam_role" "eks_cluster_role" {
   name = "${var.cluster_name}-cluster-role"
@@ -8,19 +8,15 @@ resource "aws_iam_role" "eks_cluster_role" {
     Version = "2012-10-17"
     Statement = [{
       Effect = "Allow"
-      Principal = {
-        Service = "eks.amazonaws.com"
-      }
-      Action = "sts:AssumeRole"
+      Principal = { Service = "eks.amazonaws.com" }
+      Action    = "sts:AssumeRole"
     }]
   })
 }
 
-# 클러스터 역할에 필수 정책 연결
 resource "aws_iam_role_policy_attachment" "cluster_policies" {
   for_each = toset([
     "arn:aws:iam::aws:policy/AmazonEKSServicePolicy",
-    "arn:aws:iam::aws:policy/AmazonEKSComputePolicy",
     "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
   ])
   role       = aws_iam_role.eks_cluster_role.name
@@ -28,7 +24,7 @@ resource "aws_iam_role_policy_attachment" "cluster_policies" {
 }
 
 ######################################
-# EKS 노드그룹 IAM 역할 생성
+# EKS Node IAM Role
 ######################################
 resource "aws_iam_role" "eks_node_role" {
   name = "${var.cluster_name}-node-role"
@@ -37,15 +33,12 @@ resource "aws_iam_role" "eks_node_role" {
     Version = "2012-10-17"
     Statement = [{
       Effect = "Allow"
-      Principal = {
-        Service = "ec2.amazonaws.com"
-      }
-      Action = "sts:AssumeRole"
+      Principal = { Service = "ec2.amazonaws.com" }
+      Action    = "sts:AssumeRole"
     }]
   })
 }
 
-# 노드그룹 역할에 필수 정책 연결
 resource "aws_iam_role_policy_attachment" "node_policies" {
   for_each = toset([
     "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
