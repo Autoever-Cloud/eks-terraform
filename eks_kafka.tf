7acb9e6c28f33b0bfb2f8b7b8c73a6e6449e9bba
jiwon <lnaura@naver.com>
2025-10-28 10:11:12 +0900

    eks workernode name setting

--- eks_kafka.tf
+++ eks_kafka.tf
@@ -17,6 +17,21 @@ resource "aws_eks_cluster" "kafka_cluster" {
     aws_vpc.solog_vpc,
   ]
 }
+# 1. EKS 노드를 위한 시작 템플릿 생성
+resource "aws_launch_template" "kafka_lt" {
+  name = "eks-kafka-lt"
+
+  instance_type = "t3.medium" 
+
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      Name        = "eks-kafka-node" 
+      ClusterName = "eks-kafka-cluster"
+      ManagedBy   = "Terraform"
+    }
+  }
+}
 
 # 2. EKS 노드 그룹 생성
 resource "aws_eks_node_group" "kafka_nodegroup" {
@@ -24,7 +39,12 @@ resource "aws_eks_node_group" "kafka_nodegroup" {
   node_group_name = "eks-kafka-nodegroup"  
   node_role_arn   = aws_iam_role.eks_kafka_node_role.arn
   subnet_ids      = [for s in aws_subnet.solog_public_subnets : s.id]
-  instance_types  = ["t3.medium"] 
+  #instance_types  = ["t3.medium"] 
+  
+  launch_template {
+    id      = aws_launch_template.kafka_lt.id
+    version = aws_launch_template.kafka_lt.latest_version
+  }
 
   # 오토스케일링 설정
   scaling_config {
@@ -34,7 +54,7 @@ resource "aws_eks_node_group" "kafka_nodegroup" {
   }
 
   tags = {
-    "Name"        = "eks-kafka-node"
+    "Name"        = "eks-kafka-asg"
     "ClusterName" = "eks-kafka-cluster" 
     "ManagedBy"   = "Terraform"         
   }
